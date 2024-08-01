603000637e8fdf354e9d44bf07e7187cf19eb0e3
prajall <prajalmhrzn@gmail.com>
2024-08-01 18:07:33 +0545

    work pls

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,45 +1,9 @@
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-import app from "../src/app";
-import { questionSchema } from "./models/questionModel.js";
-import { scoreSchema } from "./models/scoreModel.js";
-import { userSchema } from "./models/userModel.js";
-import { v2 as cloudinary } from "cloudinary";
+const express = require("express");
+const app = express();
 
-dotenv.config();
+app.get("/", (req, res) => res.send("server is working"));
 
-app.listen(process.env.PORT, async () => {
-  console.log("Server is running on port", process.env.PORT);
-});
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
-// connect to database
-let questionDB;
-let quizDB;
-try {
-  questionDB = mongoose.createConnection(process.env.DATABASE1_URI);
-  questionDB.on("connected", () => {
-    console.log("Database1 Connected Successfully");
-  });
-
-  quizDB = mongoose.createConnection(process.env.DATABASE2_URI);
-
-  quizDB.on("connected", () => {
-    console.log("Database2 Connected Successfully");
-  });
-
-  quizDB.on("error", (error) => {
-    console.log("Database2 Connection Error: ", error);
-  });
-} catch (err) {
-  console.log(err);
-}
-
-//assign each model to databases
-export const Question = questionDB.model("Questions", questionSchema);
-export const Score = quizDB.model("Score", scoreSchema);
-export const User = quizDB.model("Users", userSchema);
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+// module.exports = app;
+export default app;
