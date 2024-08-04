7779089f1a24e28093a6b60e66a3d21f3b2ef7d8
prajall <prajalmhrzn@gmail.com>
2024-08-04 15:14:08 +0545

    check cookies from server

--- frontend/src/authChecker.js
+++ frontend/src/authChecker.js
@@ -1,9 +1,11 @@
-import { cookies } from "next/headers";
+import axios from "axios";
 
-export const authChecker = () => {
-  const userToken = cookies().get("token");
-  if (userToken) {
-    return userToken;
+export const authChecker = async () => {
+  const user = await axios.post(
+    `${process.env.NEXT_PUBLIC_API_URL}/user/getUserInfo`
+  );
+  if (user) {
+    return user;
   } else {
     return false;
   }
