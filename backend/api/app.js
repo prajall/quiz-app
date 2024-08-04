4365e5de8f5bbd7c1ed8b1c4ba60ba2e3a8a936a
prajall <prajalmhrzn@gmail.com>
2024-08-04 12:05:45 +0545

    dasdasdasdasdsd

--- backend/api/app.js
+++ backend/api/app.js
@@ -1,30 +1,22 @@
 import express from "express";
 
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import userRoute from "./routes/userRoute.js";
-import questionRoute from "./routes/questionRoute.js";
-import scoreRoute from "./routes/scoreRoute.js";
-import leaderboardRoute from "./routes/leaderboardRoute.js";
-import { apiKeyValidation } from "./middlewares/apiKeyMiddleware.js";
-
 const app = express();
 
 export default app;
 
-app.use(
-  cors({
-    // origin: process.env.CORS_ORIGIN,
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PATCH", "DELETE"],
-    credentials: true,
-  })
-);
-app.use(express.json());
-app.use(cookieParser());
+// app.use(
+//   cors({
+//     // origin: process.env.CORS_ORIGIN,
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PATCH", "DELETE"],
+//     credentials: true,
+//   })
+// );
+// app.use(express.json());
+// app.use(cookieParser());
 
-// SETUP ROUTES
-app.use("/user", userRoute);
-app.use("/question", questionRoute);
-app.use("/score", scoreRoute);
-app.use("/leaderboard", apiKeyValidation, leaderboardRoute);
+// // SETUP ROUTES
+// app.use("/user", userRoute);
+// app.use("/question", questionRoute);
+// app.use("/score", scoreRoute);
+// app.use("/leaderboard", apiKeyValidation, leaderboardRoute);
