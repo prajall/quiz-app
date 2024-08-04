6c98f470ced38857d66c28d2c0d772140f457980
prajall <prajalmhrzn@gmail.com>
2024-08-04 14:29:53 +0545

    D

--- backend/api/app.js
+++ backend/api/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [process.env.CORS_ORIGIN],
+    origin: process.env.CORS_ORIGIN,
     methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true,
   })
