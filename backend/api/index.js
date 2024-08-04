8474f27dafc1716b26c414c85cf288a53162b5d7
prajall <prajalmhrzn@gmail.com>
2024-08-04 11:31:23 +0545

    default index

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,81 +1,8 @@
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-import { questionSchema } from "./models/questionModel.js";
-import { scoreSchema } from "./models/scoreModel.js";
-import { userSchema } from "./models/userModel.js";
-import { v2 as cloudinary } from "cloudinary";
-import app from "../src/app.js";
-
 import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import userRoute from "./routes/userRoute.js";
-import questionRoute from "./routes/questionRoute.js";
-import scoreRoute from "./routes/scoreRoute.js";
-import leaderboardRoute from "./routes/leaderboardRoute.js";
-import { apiKeyValidation } from "./middlewares/apiKeyMiddleware.js";
-
-dotenv.config();
-
 const app = express();
 
-app.use(
-  cors({
-    // origin: process.env.CORS_ORIGIN,
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PATCH", "DELETE"],
-    credentials: true,
-  })
-);
-app.use(express.json());
-app.use(cookieParser());
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-// SETUP ROUTES
-app.use("/user", userRoute);
-app.use("/question", questionRoute);
-app.use("/score", scoreRoute);
-app.use("/leaderboard", apiKeyValidation, leaderboardRoute);
-app.get("/", (req, res) => {
-  return res.send("Server is working");
-});
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
 export default app;
-
-app.listen(process.env.PORT, async () => {
-  console.log("Server is running on port", process.env.PORT);
-});
-app.get("/", (req, res) => {
-  return res.send("Server is working");
-});
-
-// connect to database
-let questionDB;
-let quizDB;
-try {
-  questionDB = mongoose.createConnection(process.env.DATABASE1_URI);
-  questionDB.on("connected", () => {
-    console.log("Database1 Connected Successfully");
-  });
-
-  quizDB = mongoose.createConnection(process.env.DATABASE2_URI);
-
-  quizDB.on("connected", () => {
-    console.log("Database2 Connected Successfully");
-  });
-
-  quizDB.on("error", (error) => {
-    console.log("Database2 Connection Error: ", error);
-  });
-} catch (err) {
-  console.log(err);
-}
-
-//assign each model to databases
-export const Question = questionDB.model("Questions", questionSchema);
-export const Score = quizDB.model("Score", scoreSchema);
-export const User = quizDB.model("Users", userSchema);
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
