e18219c140deb7a70b44a3315318db665a0edffb
Prajal Maharjan <prajalmhrzn@gmail.com>
2024-10-22 16:54:14 +0545

    cors header 1

--- backend/api/app.js
+++ backend/api/app.js
@@ -26,13 +26,13 @@ app.use(
   })
 );
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin); // or specify exact domains
+  res.header("Access-Control-Allow-Origin", "https://exams.etutorclass.com");
   res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, PUT");
   res.header(
     "Access-Control-Allow-Headers",
     "Content-Type, Authorization, x-api-key"
   );
-  res.header("Access-Control-Allow-Credentials", "true"); // Allow cookies and credentials
+  res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
 app.use(express.json());
